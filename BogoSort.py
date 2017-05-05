d20e4bde28ad1ea94cce1d4d84c204ebddbb0c57
Nathan Andersen <nathan.andersens@gmail.com>
2017-05-05 11:39:14 +0200

    hmm

--- BogoSort.py
+++ BogoSort.py
@@ -6,7 +6,9 @@ def bogosort(xs,key):
   
  def is_in_order(xs):
   for index,value in enumerate(xs):
-    for val in xs[index+1:]:
-      if value > val:
-        return false
+    if not reduce(lambda x,y: x or y,map(lambda x: x < value, xs[index+1:])):
+       return false
+#    for val in xs[index+1:]:
+ #     if value > val:
+  #      return false
  return true
