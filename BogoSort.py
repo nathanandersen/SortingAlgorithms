f9fb426a37c76615f9ebb98632c249b20948edec
Nathan Andersen <nathan.andersens@gmail.com>
2017-05-05 11:46:20 +0200

    go big or go home

--- BogoSort.py
+++ BogoSort.py
@@ -1,14 +1,18 @@
+from functools import reduce
+
 def bogosort(xs,key):
-  while not is_in_order(xs):
-    shuffle(xs)
-   return xs
-  
-  
- def is_in_order(xs):
-  for index,value in enumerate(xs):
-    if not reduce(lambda x,y: x or y,map(lambda x: x < value, xs[index+1:])):
-       return false
-#    for val in xs[index+1:]:
- #     if value > val:
-  #      return false
- return true
+    while not is_in_order(xs):
+        shuffle(xs)
+    return xs
+
+
+def is_in_order(xs):
+    for index,value in enumerate(xs[:-1]):
+        if reduce(lambda x,y: x or y,map(lambda x: x < value, xs[index+1:])):
+            # because when you're killing an ant with a missile
+            # go big or go home
+            return False
+#        for val in xs[index+1:]
+#            if value > val:
+#                return false
+    return True
